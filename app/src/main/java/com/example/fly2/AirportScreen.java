9cb7058580588594ade602a41d06c09469c8d5a3
Emily Lauren Roth <emilylroth@utexas.edu>
2018-11-28 16:22:14 -0600

    UI stuff

--- app/src/main/java/com/example/fly2/AirportScreen.java
+++ app/src/main/java/com/example/fly2/AirportScreen.java
@@ -3,9 +3,13 @@ package com.example.fly2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatAutoCompleteTextView;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 // First screen allowing users to select the airports then go to next screen
 public class AirportScreen extends AppCompatActivity {
@@ -14,14 +18,25 @@ public class AirportScreen extends AppCompatActivity {
     EditText airport2Input;
     Airport airport1;
     Airport airport2;
+    Spinner airportSelection1;
+    Spinner airportSelection2;
+
+    private static final String[] COUNTRIES = new String[] {
+            "Belgium", "France", "Italy", "Germany", "Spain"
+    }; // TODO: Make this a res file for cleanliness
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_airport_screen);
-        airport1Input = (EditText) findViewById(R.id.airport1input);
-        airport2Input = (EditText) findViewById(R.id.airport2input);
-        // TODO: turn into airport objects
+        airportSelection1 = (Spinner) findViewById(R.id.spinnerAirport1);
+        airportSelection2 = (Spinner) findViewById(R.id.spinnerAirport2);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(AirportScreen.this, android.R.layout.simple_expandable_list_item_1, getResources().getStringArray(R.array.airport_cities));
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        airportSelection1.setAdapter(adapter);
+        airportSelection2.setAdapter(adapter);
+
 
         // TODO: Do the autocomplete stuff by making an array in resources then connecting it (https://www.tutorialspoint.com/android/android_auto_complete.html)
         final Button btn = (Button)findViewById(R.id.btnToDatePicker);
@@ -35,8 +50,8 @@ public class AirportScreen extends AppCompatActivity {
     }
 
     public void pickDates(){
-        airport1 = new Airport(String.valueOf(airport1Input.getText()));
-        airport2 = new Airport(String.valueOf(airport2Input.getText()));
+        airport1 = new Airport(airportSelection1.getSelectedItem().toString());
+        airport2 = new Airport(airportSelection2.getSelectedItem().toString());
 
         if(airport1 == null || airport2 == null){
             System.out.print("airport is null don't do this fam");
