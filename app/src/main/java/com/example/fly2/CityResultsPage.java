679b04e1ed31b358410d336e871d930688179975
mblume97 <mablume97@gmail.com>
2018-12-05 16:34:06 -0600

    started populating the results page

--- app/src/main/java/com/example/fly2/CityResultsPage.java
+++ app/src/main/java/com/example/fly2/CityResultsPage.java
@@ -7,6 +7,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -22,26 +23,31 @@ import com.loopj.android.http.JsonHttpResponseHandler;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Locale;
 
 // results that allows users to pick what city to go to
 public class CityResultsPage extends AppCompatActivity {
 
-    private double firstPrice;
-    private double secondPrice;
+    private Double firstPrice;
+    private Double secondPrice;
     private String city;
     private String airportCode;
     private String airportName;
     private String firstAirline;
     private String secondAirline;
     private String airline;
-    private String url;
     private AsyncHttpClient client;
     private ImageView imageView;
     private RequestQueue mQueue;
+    private TextView firstHotelView;
+    private TextView secondHotelView;
+    private TextView thirdHotelView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +58,7 @@ public class CityResultsPage extends AppCompatActivity {
         mQueue = Volley.newRequestQueue(this);
 
         firstPrice = extras.getDouble("firstPrice");
+
         secondPrice = extras.getDouble("secondPrice");
         city = extras.getString("city");
         airportCode = extras.getString("code");
@@ -60,18 +67,35 @@ public class CityResultsPage extends AppCompatActivity {
         secondAirline = extras.getString("secondAirline");
 
 
+        TextView destinationTextView = (TextView)findViewById(R.id.destinationCity);
+        destinationTextView.setText(city);
+
+        TextView firstPriceView = (TextView)findViewById(R.id.firstPrice);
+        firstPriceView.setText(String.format(Locale.US, "$%.2f", firstPrice));
+
+        TextView secondPriceView = (TextView)findViewById(R.id.secondPrice);
+        secondPriceView.setText(String.format(Locale.US, "$%.2f", secondPrice));
+
+        TextView firstAirlineView = (TextView)findViewById(R.id.firstAirline);
+        firstAirlineView.setText(firstAirline);
+
+        TextView secondAirlineView = (TextView)findViewById(R.id.secondAirline);
+        secondAirlineView.setText(secondAirline);
+
+        firstHotelView = (TextView)findViewById(R.id.firstHotel);
+        secondHotelView = (TextView)findViewById(R.id.secondHotel);
+        thirdHotelView = (TextView)findViewById(R.id.thirdHotel);
+        imageView = findViewById(R.id.imageView);
+
         // TODO: if either price is infinity, display there are no flights to any destination
 
-        this.client = new AsyncHttpClient();
-        url = PhotoClient.createURL("atlanta");
-        imageView = findViewById(R.id.imageView);
-        displayImage();
+        displayImage(city.split(",")[0]);
         getHotels(city);
     }
 
 
-    private void displayImage() {
-        String url = "https://api.unsplash.com/search/photos?page=1&query=Houston&client_id=6651e9c60ee7bc00749d8ae180359ffcccda0500a4a49bde116b48d7c3407223";
+    private void displayImage(String city) {
+        String url = "https://api.unsplash.com/search/photos?page=1&query=" + city + "&client_id=6651e9c60ee7bc00749d8ae180359ffcccda0500a4a49bde116b48d7c3407223";
     JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
         (Request.Method.GET, url, (String)null, new Response.Listener<JSONObject>() {
 
@@ -110,10 +134,15 @@ public class CityResultsPage extends AppCompatActivity {
                     public void onResponse(JSONObject response) {
                         try {
                             JSONArray venues = response.getJSONObject("response").getJSONArray("venues");
+                            ArrayList<String> temp = new ArrayList<>();
                             for (int i = 0; i < 3; i++) {
                                 JSONObject venue = venues.getJSONObject(i);
                                 String name = venue.getString("name");
+                                temp.add(name);
                             }
+                            firstHotelView.setText(temp.get(0));
+                            secondHotelView.setText(temp.get(1));
+                            thirdHotelView.setText(temp.get(2));
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
